abaf35baf2b5040064531fffdf5c9cb434e2a20a
Oleksandr Kuzmenko <oleksandr.kuzmenko@rocketroute.com>
2021-12-01 00:00:27 +0200

    first commit

---
+++ card_game/main.js
@@ -0,0 +1,20 @@
+;(function(window){
+
+	var Game = function(el, option){
+		
+	}
+
+	var Deck = function(deck_div, option){
+		
+	}
+	
+	var Card = function(){
+
+	}
+	
+	var DiscardPile = function(){
+
+	}
+	
+	window.Game = Game;
+})(window);
